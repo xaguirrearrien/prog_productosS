3f927df272ef6f3df6704f781d3f871317b32f7c
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-03-30 09:44:24 +0200

    ModelDescuento first commit

---
+++ src/modelo/dao/ModeloDescuento.java
@@ -0,0 +1,14 @@
+package modelo.dao;
+
+import java.util.ArrayList;
+
+import modelo.bean.Descuento;
+
+public class ModeloDescuento {
+	
+	public ArrayList<Descuento> getAll(){
+		//TODO implementar metodo que devuelva todos los estados
+		return null;
+	}
+
+}
